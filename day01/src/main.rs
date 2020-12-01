a8f5181dfcbb5daaf6fbcf19db02f228f090a4d5
Paul Unger <g.paul.u@gmail.com>
2020-12-01 16:30:21 -0500

    impl algorithm to pass example test

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,8 +2,16 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn find_sum_2020(list: &[i32]) -> (i32, i32) {
-    (0, 0)
+// returns smaller number first
+fn find_sum_2020(mut list: Vec<i32>) -> Option<(i32, i32)> {
+    list.sort_unstable();
+    for i in &list {
+        let needle = 2020 - *i;
+        if let Ok(index) = list.binary_search(&needle) {
+            return Some((*i, list[index]));
+        }
+    }
+    None
 }
 
 #[cfg(test)]
@@ -11,8 +19,8 @@ mod tests {
     use super::*;
     #[test]
     fn example() {
-        let found = find_sum_2020(&[1721, 979, 366, 299, 675, 1456]);
-        assert_eq!(found, (1721, 299));
+        let found = find_sum_2020([1721, 979, 366, 299, 675, 1456].into()).unwrap();
+        assert_eq!(found, (299, 1721));
         assert_eq!(found.0 * found.1, 514579);
     }
 }
