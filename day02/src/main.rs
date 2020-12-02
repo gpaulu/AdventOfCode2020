c47f0cc4fd5f889947b4662cddf43076eda2f2b0
Paul Unger <g.paul.u@gmail.com>
2020-12-02 16:39:14 -0500

    unit test for day02 part1 example

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -2,11 +2,16 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn num_passwords_valid(passwords: &str) -> i32 {
+    -1
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn example() {
-        assert_eq!(0, 1);
+        let input = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc";
+        assert_eq!(num_passwords_valid(input), 2);
     }
 }
