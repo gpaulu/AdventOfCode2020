e0e1949faac60db29aef4a90661c6f957da31ab2
Paul Unger <g.paul.u@gmail.com>
2020-12-05 18:57:33 -0500

    find missing seat id algo and test

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("Max seat id: {}", max_seat_id(&passes));
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 struct Seat {
     row: i32,
     column: i32,
@@ -62,6 +62,29 @@ fn max_seat_id(passes: &str) -> i32 {
         .id
 }
 
+fn find_empty_seat_id(passes: &str) -> Option<i32> {
+    let ids: Vec<_> = passes.lines().map(|s| decode_boarding_pass(s).id).collect();
+    find_missing_in_list(ids)
+}
+
+fn find_missing_in_list(mut list: Vec<i32>) -> Option<i32> {
+    list.sort_unstable();
+    let mut prev: Option<i32> = None;
+    let mut found: Option<i32> = None;
+    for current in list {
+        if let Some(prev) = prev {
+            if current - 1 != prev {
+                if found.is_some() {
+                    panic!("multiple numbers missing");
+                }
+                found = Some(prev + 1);
+            }
+        }
+        prev = Some(current);
+    }
+    found
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -109,5 +132,17 @@ mod tests {
     fn found_max_seat_id() {
         let passes = "FBFBBFFRLR\nBFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL\n";
         assert_eq!(max_seat_id(passes), 820);
+    }
+
+    #[test]
+    fn found_missing_number() {
+        assert_eq!(find_missing_in_list(vec![2, 3, 4, 6, 7]).unwrap(), 5);
+        assert_eq!(find_missing_in_list(vec![7, 3, 6, 4, 2]).unwrap(), 5);
+        assert!(find_missing_in_list(vec![2, 3, 4, 6, 5]).is_none());
+    }
+    #[test]
+    #[should_panic(expected = "multiple numbers missing")]
+    fn multiple_missing_numbers() {
+        find_missing_in_list(vec![7, 3, 6, 9, 4, 2]);
     }
 }
