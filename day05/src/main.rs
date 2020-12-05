74fa9db9207605cf744d53888e805aa0d052343c
Paul Unger <g.paul.u@gmail.com>
2020-12-05 18:13:30 -0500

    impl algorithm to decode passes and pass tests

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -10,7 +10,44 @@ struct Seat {
 }
 
 fn decode_boarding_pass(pass: &str) -> Seat {
-    todo!()
+    // This algorithm is obtuse and inefficient in Rust
+    // But, hey, it works!
+    // And it was fun to figure out how to make it work
+    fn range_binary_search(
+        range: Box<dyn ExactSizeIterator<Item = u16>>,
+        c: char,
+        left_char: char,
+        right_char: char,
+    ) -> Box<dyn ExactSizeIterator<Item = u16>> {
+        let len = range.len();
+        if c == left_char {
+            Box::new(range.take(len / 2))
+        } else if c == right_char {
+            Box::new(range.skip(len / 2))
+        } else {
+            panic!("Bad encoding")
+        }
+    }
+    let row = pass
+        .chars()
+        .take(7)
+        .fold(
+            Box::new(0..=127u16) as Box<dyn ExactSizeIterator<Item = u16>>,
+            |range, c| range_binary_search(range, c, 'F', 'B'),
+        )
+        .next()
+        .unwrap() as i32;
+    let column = pass
+        .chars()
+        .skip(7)
+        .fold(
+            Box::new(0..=7u16) as Box<dyn ExactSizeIterator<Item = u16>>,
+            |range, c| range_binary_search(range, c, 'L', 'R'),
+        )
+        .next()
+        .unwrap() as i32;
+    let id = row * 8 + column;
+    Seat { row, column, id }
 }
 
 #[cfg(test)]
