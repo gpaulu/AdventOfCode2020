84244a67b1559ffc14f1282ba7fc660722c82568
Paul Unger <g.paul.u@gmail.com>
2020-12-06 17:49:38 -0500

    set up tests for part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -20,6 +20,14 @@ fn num_yes_to_questions_in_group(answers: &str) -> usize {
     set.len()
 }
 
+fn sum_groups_all_yes(answers: &str) -> usize {
+    todo!()
+}
+
+fn num_all_yes_to_questions_in_group(answers: &str) -> usize {
+    todo!()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -54,6 +62,45 @@ c";
         let input = "ab
 ac";
         assert_eq!(num_yes_to_questions_in_group(input), 3);
+        let input = "a
+a
+a
+a";
+        assert_eq!(num_yes_to_questions_in_group(input), 1);
+        let input = "b";
+        assert_eq!(num_yes_to_questions_in_group(input), 1);
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+        assert_eq!(sum_groups_all_yes(input), 6);
+    }
+    #[test]
+    fn part2_single_group() {
+        let input = "abc";
+        assert_eq!(num_yes_to_questions_in_group(input), 3);
+        let input = "a
+b
+c";
+        assert_eq!(num_yes_to_questions_in_group(input), 0);
+        let input = "ab
+ac";
+        assert_eq!(num_yes_to_questions_in_group(input), 1);
         let input = "a
 a
 a
