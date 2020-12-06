94bde5e1b03f44f8c3144d2947deeb5b7bb0582c
Paul Unger <g.paul.u@gmail.com>
2020-12-06 17:34:46 -0500

    mores tests

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -6,6 +6,10 @@ fn sum_groups_yes(answers: &str) -> usize {
     todo!()
 }
 
+fn num_yes_to_questions_in_group(answers: &str) -> usize {
+    todo!();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,5 +32,24 @@ a
 
 b";
         assert_eq!(sum_groups_yes(input), 11);
+    }
+    #[test]
+    fn single_group() {
+        let input = "abc";
+        assert_eq!(num_yes_to_questions_in_group(input), 3);
+        let input = "a
+b
+c";
+        assert_eq!(num_yes_to_questions_in_group(input), 3);
+        let input = "ab
+ac";
+        assert_eq!(num_yes_to_questions_in_group(input), 3);
+        let input = "a
+a
+a
+a";
+        assert_eq!(num_yes_to_questions_in_group(input), 1);
+        let input = "b";
+        assert_eq!(num_yes_to_questions_in_group(input), 1);
     }
 }
