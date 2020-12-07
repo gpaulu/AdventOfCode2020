48f408a050faafcad3b96f3f1ccfd29307091a7c
Paul Unger <g.paul.u@gmail.com>
2020-12-06 20:20:41 -0500

    pass all part 2 tests

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -21,11 +21,36 @@ fn num_yes_to_questions_in_group(answers: &str) -> usize {
 }
 
 fn sum_groups_all_yes(answers: &str) -> usize {
-    todo!()
+    answers
+        .split("\n\n")
+        .map(num_all_yes_to_questions_in_group)
+        .sum()
 }
 
 fn num_all_yes_to_questions_in_group(answers: &str) -> usize {
-    todo!()
+    let sets = answers
+        .lines()
+        .map(|s| s.chars().collect::<HashSet<_>>())
+        .collect::<Vec<_>>();
+    intersection_many_sets(&sets).unwrap().count()
+}
+
+// `HashSet` has fn `intersection` but only gives the result for 2 sets
+// This fn takes an iterator of `HashSet`s and returns `Some` iterator of intersecting elements
+// returns `None` if input iterator `sets` results in 0 `HashSet`s
+fn intersection_many_sets<'iter, T>(
+    sets: &'iter [HashSet<T>],
+) -> Option<impl Iterator<Item = &'iter T>>
+where
+    T: std::cmp::Eq,
+    T: std::hash::Hash,
+    T: 'iter,
+{
+    sets.iter().next().map(move |first_set| {
+        first_set
+            .iter()
+            .filter(move |item| sets.iter().all(|s| s.contains(*item)))
+    })
 }
 
 #[cfg(test)]
@@ -108,5 +133,19 @@ a";
         assert_eq!(num_all_yes_to_questions_in_group(input), 1);
         let input = "b";
         assert_eq!(num_all_yes_to_questions_in_group(input), 1);
+    }
+
+    #[test]
+    fn set_intersection() {
+        let sets: &[HashSet<i32>] = &[
+            [1, 2, 3].iter().cloned().collect(),
+            [1, 4, 5].iter().cloned().collect(),
+            [1, 6, 7].iter().cloned().collect(),
+        ];
+        let intersection = intersection_many_sets(sets);
+        assert!(intersection.is_some());
+        let mut intersection = intersection.unwrap();
+        assert_eq!(intersection.next(), Some(&1));
+        assert_eq!(intersection.next(), None);
     }
 }
