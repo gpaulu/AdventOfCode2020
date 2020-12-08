6cadeba3859ab1060e5616fe9193ee042d0d902b
Paul Unger <g.paul.u@gmail.com>
2020-12-08 01:36:54 -0500

    pass tests for part 1

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,16 +1,21 @@
 use std::collections::HashMap;
 
-use petgraph::prelude::*;
+use petgraph::{prelude::*, visit::Walker};
 
 fn main() {
     println!("Hello, world!");
 }
 
 fn num_colors_can_contain(rules: &str, color: &str) -> usize {
-    todo!()
+    let graph = parse_rules(rules);
+    let start = graph.get_index(color);
+    petgraph::visit::Bfs::new(&graph.graph, start)
+        .iter(petgraph::visit::Reversed(&graph.graph))
+        .skip(1) // first is our starting node
+        .count()
 }
 
-fn parse_rules(rules: &str) -> DiGraph<&str, usize> {
+fn parse_rules(rules: &str) -> Graph {
     let mut graph = Graph::default();
     for rule in rules.lines() {
         let mut mapping = rule.split(" bags contain ");
@@ -20,14 +25,13 @@ fn parse_rules(rules: &str) -> DiGraph<&str, usize> {
                 break;
             }
             let first_space = child.find(' ').unwrap();
-            println!("Parsing: {}", child);
             let num = child[..first_space].parse::<usize>().unwrap();
             let bag = child.find(" bag").unwrap();
             let child_color = &child[first_space + 1..bag];
             graph.update(first_color, child_color, num);
         }
     }
-    graph.graph
+    graph
 }
 
 #[derive(Debug, Default)]
@@ -38,10 +42,6 @@ struct Graph<'a> {
 
 impl<'a> Graph<'a> {
     fn update(&mut self, from: &'a str, to: &'a str, num: usize) {
-        println!(
-            "Graph::update(from: \"{}\", to \"{}\", num: {}",
-            from, to, num
-        );
         let index1 = self.insert_node(from);
         let index2 = self.insert_node(to);
         self.graph.add_edge(index1, index2, num);
@@ -52,6 +52,9 @@ impl<'a> Graph<'a> {
             .node_map
             .entry(node)
             .or_insert_with(|| graph_ref.add_node(node))
+    }
+    fn get_index(&self, node: &'a str) -> NodeIndex {
+        self.node_map[node]
     }
 }
 
@@ -82,7 +85,7 @@ muted yellow bags contain no other bags.";
         let by = g.add_node("bright yellow");
         let my = g.add_node("muted yellow");
         g.extend_with_edges(&[(lr, by, 1), (lr, my, 2)]);
-        let rules = parse_rules(input);
+        let rules = parse_rules(input).graph;
         assert_eq!(rules.node_count(), g.node_count());
     }
 }
