f179a05f5aaee27566b53462160f16efcf542c20
Paul Unger <g.paul.u@gmail.com>
2020-12-07 22:47:16 -0500

    set up test for day07 part 1

---
+++ day07/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn num_colors_can_contain(rules: &str, color: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = "light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.";
+        assert_eq!(num_colors_can_contain(input, "shiny gold"), 4);
+    }
+}
