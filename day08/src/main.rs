b853d68efd72592d1d76c486c5d028a40cc4044d
Paul Unger <g.paul.u@gmail.com>
2020-12-08 22:59:40 -0500

    refactor part 1 for easier use in part 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -8,37 +8,74 @@ fn main() {
 }
 
 fn acc_val_before_loop(program: &str) -> i32 {
-    let program = parse_program(program);
-    let mut accumulator = 0;
-    let mut counter = 0usize;
-    let mut visited = HashSet::new();
-    while counter < program.len() {
-        let line = program[counter];
-        if visited.contains(&line) {
-            break;
-        } else {
-            visited.insert(line);
-        }
-        match line.instruction {
-            Instruction::Nop(_) => counter += 1,
-            Instruction::Acc(arg) => {
-                accumulator += arg;
-                counter += 1
-            }
-            Instruction::Jmp(arg) => {
-                if arg < 0 {
-                    counter -= arg.abs() as usize
-                } else {
-                    counter += arg as usize
-                }
-            }
-        }
-    }
-    accumulator
+    let mut program: Program = program.parse().unwrap();
+    program.run().unwrap_err()
 }
 
 fn acc_fixed(program: &str) -> i32 {
     todo!()
+}
+
+#[derive(Debug, Clone, Default)]
+struct Program {
+    instructions: Vec<ProgramLine>,
+    counter: usize,
+    accumulator: i32,
+}
+
+impl Program {
+    fn execute_instruction(&mut self) {
+        let line = self.instructions[self.counter];
+        match line.instruction {
+            Instruction::Nop(_) => self.counter += 1,
+            Instruction::Acc(arg) => {
+                self.accumulator += arg;
+                self.counter += 1
+            }
+            Instruction::Jmp(arg) => {
+                if arg < 0 {
+                    self.counter -= arg.abs() as usize
+                } else {
+                    self.counter += arg as usize
+                }
+            }
+        }
+    }
+
+    //returns Ok(accumulator) if program terminates successfully
+    //else returns Err(accumulator) if a loop is detected
+    fn run(&mut self) -> Result<i32, i32> {
+        let mut visited = HashSet::new();
+        while self.counter < self.instructions.len() {
+            let line = self.instructions[self.counter];
+            if visited.contains(&line) {
+                return Err(self.accumulator);
+            } else {
+                visited.insert(line);
+            }
+            self.execute_instruction();
+        }
+        Ok(self.accumulator)
+    }
+}
+
+impl FromStr for Program {
+    type Err = BoxError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let instructions = s
+            .lines()
+            .enumerate()
+            .map(|(num, line)| ProgramLine {
+                line_number: num,
+                instruction: line.parse().unwrap(),
+            })
+            .collect();
+        Ok(Program {
+            instructions,
+            ..Default::default()
+        })
+    }
 }
 
 fn parse_program(program: &str) -> Vec<ProgramLine> {
